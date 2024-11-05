37dae84e9bd0964ff07e50445adf99d84677e388
ClaytonWas <claywasmund@gmail.com>
2024-11-04 21:11:41 -0500

    Added Scene Obstacle Objects and Toolbar For Viewing Angles.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,25 +4,43 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer()
 camera.position.x = 0
 camera.position.y = 15
-camera.position.z = 5
+camera.position.z = 15
 camera.rotation.x = -0.6
+
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 
-// Adding A Mesh To The Three.js Scene
+// Adding A Floorplane To The Three.js Scene
 const gameWindow = document.getElementById('gameWindow')
 renderer.setSize(gameWindow.clientWidth, gameWindow.clientHeight)
 gameWindow.appendChild(renderer.domElement)
-const floor1 = new THREE.PlaneGeometry(20, 10)
+const floor1Geometry = new THREE.PlaneGeometry(20, 10)
 const floor1Material = new THREE.MeshBasicMaterial({ color: 0xaaaaaa, side: THREE.DoubleSide })
-const floor1Mesh = new THREE.Mesh(floor1, floor1Material)
+const floor1Mesh = new THREE.Mesh(floor1Geometry, floor1Material)
 floor1Mesh.position.set(0, 0, -10)
 floor1Mesh.rotation.set(-Math.PI/2, 0, 0)
 
-// Three.js Mesh Array For A Worlds Floorplan
-const meshes = new THREE.Group()
-meshes.add(floor1Mesh)
-scene.add(meshes)
+// Three.js Mesh Array For Worlds Floorplan
+const floors = new THREE.Group()
+floors.add(floor1Mesh)
+scene.add(floors)
+
+// Adding Objects To The Three.js Scene
+const box1Geometry = new THREE.BoxGeometry(2, 1, 2)
+const box1Material = new THREE.MeshBasicMaterial({ color: 0x12ABC1 })
+const box1Mesh = new THREE.Mesh(box1Geometry, box1Material)
+box1Mesh.position.set(5, 0.5, -10)
+
+const box2Geometry = new THREE.BoxGeometry(1, 2, 1)
+const box2Material = new THREE.MeshBasicMaterial({ color: 0xAA1100 })
+const box2Mesh = new THREE.Mesh(box2Geometry, box2Material)
+box2Mesh.position.set(-7, 1, -10)
+
+// Three.js Mesh Array For Impassable Objects
+const objects = new THREE.Group()
+objects.add(box1Mesh)
+objects.add(box2Mesh)
+scene.add(objects)
 
 //User attributes
 let userId;
@@ -39,18 +57,15 @@ function onMouseClick(event) {
     mouse.y = -((event.clientY - gameWindow.getBoundingClientRect().top) / gameWindow.clientHeight) * 2 + 1;
     raycaster.setFromCamera(mouse, camera)
 
-    const intersects = raycaster.intersectObjects(meshes.children)          // Checks The Global Meshes Object To See If It Intersects With Any Meshes In The Scene
+    const intersects = raycaster.intersectObjects(floors.children)          // Checks The Global Meshes Object To See If It Intersects With Any Meshes In The Scene
 
     if (intersects.length > 0) {
         const point = intersects[0].point           // Grabs the first point of intersection.
 
         if (point) {
-            movePlayer(socket.user.id, point)
             socket.emit('updatePlayerPosition', point)
         }
     }
-
-    
 }
 
 function movePlayer(playerId, point) { 
@@ -100,25 +115,6 @@ function animate() {
     renderer.setSize(gameWindow.clientWidth, gameWindow.clientHeight)
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-}
-
-// Function That Casts A Ray To Intersect With The Closest Object At That Point From The Camera
-function onMouseClick(event) {
-    mouse.x = ((event.clientX - gameWindow.getBoundingClientRect().left) / gameWindow.clientWidth) * 2 - 1;
-    mouse.y = -((event.clientY - gameWindow.getBoundingClientRect().top) / gameWindow.clientHeight) * 2 + 1;
-    raycaster.setFromCamera(mouse, camera)
-
-    const intersects = raycaster.intersectObjects(meshes.children)          // Checks The Global Meshes Object To See If It Intersects With Any Meshes In The Scene
-
-    if (intersects.length > 0) {
-        const point = intersects[0].point           // Grabs the first point of intersection.
-
-        if (point) {
-            socket.emit('updatePlayerPosition', point)
-        }
-    }
-
-    
 }
 
 function linearMovement(playerId, targetPoint, duration = 1000) {
@@ -187,8 +183,62 @@ function instantiatePlayer(id, name, color, position){
     scene.add(clientPlayer.mesh)
 }
 
+// Toolbar Handler
+const toolbarInputs = {
+    '1': () => {
+        console.log('Camera Angle 1 - Game Angle');
+        camera.position.x = 0
+        camera.position.y = 15
+        camera.position.z = 15
+
+        camera.rotation.x = -0.6
+        camera.rotation.y = 0
+        camera.rotation.z = 0
+    },
+    '2': () => {
+        console.log('Camera Angle 2 - Perspective Angle ');
+        camera.position.x = -10
+        camera.position.y = 5
+        camera.position.z = 10
+
+        camera.rotation.x = 0
+        camera.rotation.y = -0.3
+        camera.rotation.z = 0
+    },
+    '3': () => {
+        console.log('Camera Angle 3 - At World Origin');
+        camera.position.x = 0
+        camera.position.y = 0
+        camera.position.z = 0
+
+        camera.rotation.x = 0
+        camera.rotation.y = 0
+        camera.rotation.z = 0
+    },    
+    '4': () => {
+        console.log('Camera Angle 4 - Near Plane Angle');
+        camera.position.x = 0
+        camera.position.y = 0.75
+        camera.position.z = 2
+
+        camera.rotation.x = 0
+        camera.rotation.y = 0
+        camera.rotation.z = 0
+    }
+};
+
 document.addEventListener('DOMContentLoaded', () => {
     gameWindow.addEventListener('click', onMouseClick)
+
+    // Toolbar Listener
+    document.addEventListener('keydown', (event) => {
+        const key = event.key;
+        
+        // Check if the pressed key has a corresponding action
+        if (toolbarInputs[key]) {
+            toolbarInputs[key]();
+        }
+    });
 
     socket.on('sendPlayerData', (player) => {
         instantiatePlayer(player.id, player.username, player.color, player.position)
