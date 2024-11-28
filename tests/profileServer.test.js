597c049bd2e48f3472498cbc35f2c3ffa2836cf2
ClaytonWas <claywasmund@gmail.com>
2024-11-28 13:41:01 -0500

    Added Login Post Test.

--- tests/profileServer.test.js
+++ tests/profileServer.test.js
@@ -1,9 +1,34 @@
 const request = require('supertest')
+const fs = require('fs')
+const path = require('path')
 const app = require('../profileServer.js')
 
 describe('GET /', () => {
-  it('should respond with a 200 status and a message', async () => {
-    const response = await request(app).get('/')
-    expect(response.status).toBe(200)
-  })
+    it('should respond with a 200 status and index.html', async () => {
+        const response = await request(app).get('/')
+        expect(response.status).toBe(200)
+        
+        let htmlContent = fs.readFileSync(path.join(__dirname, '../views/index.html'), 'utf-8')
+        expect(response.text).toBe(htmlContent)
+    })
+})
+
+describe('GET /login', () => {
+    it('should respond with a 200 status and login.html', async () => {
+        const response = await request(app).get('/login')
+        expect(response.status).toBe(200)
+        
+        let htmlContent = fs.readFileSync(path.join(__dirname, '../views/login.html'), 'utf-8')
+        expect(response.text).toBe(htmlContent)
+    })
+})
+
+describe('POST /login', () => {
+    it('should respond with a 200 status and return a signed token', async () => {
+        const knownGoodAccount = {username: 'clay', password: 'foobar'}
+        const response = await request(app).post('/login')
+        .send(knownGoodAccount)
+        expect(response.status).toBe(200)
+        expect(response.text).toContain('token')
+    })
 })
